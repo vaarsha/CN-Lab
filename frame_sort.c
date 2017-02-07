1134c105a2f277d8b6df4abae0f4887986021b7f
Varsha Rao <varshar186@gmail.com>
2017-02-08 00:46:10 +0530

    Dividing the input string into different frame size

--- frame_sort.c
+++ frame_sort.c
@@ -2,10 +2,15 @@
 #include<string.h>
 #include<stdlib.h>
 #define maxsz 21
+
 int main()
 {
+	int fsz,i,j,k=0,z=0;
+	struct frame {
+		int id;
+		char st[20];
+	};
 	//Allocate memory and check it's availability.
-	int fsz,i;
 	char *s = malloc(maxsz);
 	if(s == NULL){
 		printf("Not enough memory\n");
@@ -16,6 +21,7 @@ int main()
 	printf("it is %s",s);
 	printf("Enter frame size:");
 	scanf("%d",&fsz);
+	struct frame fr[fsz];
 	if(fsz>strlen(s))
 		printf("Invalid frame size\n");
 	else
@@ -24,6 +30,16 @@ int main()
 		for(i=0; i<strlen(s) && s[i]!='\n'; i++){
 			printf("%c\n", s[i]);
 		}
+		for(j=0;j<=fsz;j++){
+			fr[j].id = j;
+			printf("here %d\n",fr[j].id);
+			k += fsz;
+			for(i=0; i<fsz &&s[z]!='\n'&& z<k;i++, z++){
+				fr[j].st[i] = s[z];
+			}
+			printf("string is %s\n",fr[j].st);
+		}
+
 	}
 	free(s);
 	return 0;
