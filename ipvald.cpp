1a7d136dac03e501111daeccc60d0b8860227327
Varsha Rao <varshar186@gmail.com>
2017-02-13 00:31:37 +0530

    Added comments.

--- ipvald.cpp
+++ ipvald.cpp
@@ -10,6 +10,7 @@ int anum[4],j=0,z=1,cnt=0,k;
 cout<<"Enter ip address"<<endl;
 cin>>s;
 cout<<"The entered ip is "<<s<<endl;
+//This checks number of dots in the string.
 for(int i = 0; i<strlen(s); i++){
 	if(s[i] == '.')
 		z +=1;
@@ -18,23 +19,27 @@ for(int i = 0; i<strlen(s); i++){
 if (strlen(s)> 6 && strlen(s)<16 && s[0]!='.' && z == 4){
     //printf("The number is %d",a-'0');
     z=0;
+    //j is the index, which points to current value of array.
     while(j<strlen(s)+1){
             k=j;
           while(!(s[k]== '.' || s[k]== '\0') && k<strlen(s)){
+	     //converts char into integer.
              num[cnt] = s[k]-'0';
-	     cnt++;
+	     cnt++; //keeps track of digits in a number.
              k++;
             }
+	    //For one digit number.
             if(cnt == 1){
                anum[z]=num[0];
                j+= 1;
                z++;
             }
+	    //For two digit number.
             else if(cnt==2){
                 anum[z]=num[0]*10+num[1];
                 j+=2;
                 z++;
-            }
+            }//For three digit number.
             else if(cnt==3){
 		anum[z]=num[0]*100+num[1]*10+num[2];
                 j+=3;
